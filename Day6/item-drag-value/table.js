70910e60f488d488865ff00456e5fbda5066ce0a
sarthak <khandelwalsarthak81@yahoo.com>
2017-07-22 21:32:45 +0530

    table changes

--- Day6/item-drag-value/table.js
+++ Day6/item-drag-value/table.js
@@ -12,35 +12,3 @@ function makeTable(){
 	}
 	myTable.appendTo("#dvTable");
 }
-
-// var myApp = myApp || {};
-
-// myApp.updateSliderRow = function (slideAmount) {
-//     var display = document.getElementById("chosenrow");
-//     var Slidisplay = document.getElementById("sliderow");
-//     Slidisplay.value = slideAmount;
-//     display.value = slideAmount;
-
-// };
-// myApp.updateSliderCol = function (slideAmount) {
-//     var display = document.getElementById("chosencol");
-//     var Slidisplay = document.getElementById("slidercol");
-//     Slidisplay.value = slideAmount;
-//     display.value = slideAmount;
-
-// };
-// myApp.createTable = function () {
-//     var mytable = $('<div id="basicTable"></div>');
-//     document.getElementById("dvTable").innerHTML = '';
-//     var rows = parseInt($("#chosenrow").val());
-//     var cols = parseInt($("#chosencol").val());
-//     for (var i = 0; i < rows; i++) {
-//         var row = $('<div></div>').appendTo(mytable);
-//         for (var j = 0; j < cols; j++) {
-//             $('<div class="colElements"></div>').text("text").appendTo(row);
-//         }
-
-//     }
-//     mytable.appendTo("#dvTable");
-
-// };
