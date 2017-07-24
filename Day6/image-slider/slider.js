56ebacb552d574bf6813fb51b3c71514b10e225d
Sarthak Khandelwal (SAKH) <sakh@saxobank.com>
2017-07-24 18:39:58 +0530

    slider changes using pattern

--- Day6/image-slider/slider.js
+++ Day6/image-slider/slider.js
@@ -1,14 +1,16 @@
+var sliderTask  = (function(){
+
 
     var slideList = document.getElementById('slideList'),
-    items = document.getElementsByClassName('items'),
-    sliderListLength = items.length,
-    counter = 0, count=0,xcoord,xcordmd,xcoordmu,
-    nextArrow = document.getElementById('date-nav-next'),
-    prevArrow = document.getElementById('date-nav-prev');
+        items = document.getElementsByClassName('items'),
+        sliderListLength = items.length,
+        counter = 0, count=0,xcoord,xcordmd,xcoordmu,
+        nextArrow = document.getElementById('date-nav-next'),
+        prevArrow = document.getElementById('date-nav-prev');
 
     nextArrow.addEventListener("click", callNext);
-    prevArrow.addEventListener("click", callPrev);
 
+    prevArrow.addEventListener("click", callPrev);
 
     function callNext(){
         if(counter < sliderListLength-1){
@@ -27,28 +29,44 @@
         }
     }
 
-    function Picture(currentPic){
-        if(event.wheelDelta >=120){
-            Resize(++count,currentPic);
-        }else if (event.wheelDelta <=-120){ 
-            Resize(--count,currentPic);
-        } 
-    }
-
-    function Resize(count,currentPic){
-        currentPic.style.zoom = c + '0%';
+    function zoomPic(thisPic){
+        console.log(event.deltaY);
+        if(event.deltaY < 0){
+            thisPic.style.transform += "scale(2,2)";
+        }
+        else{
+            thisPic.style.transform += "scale(0.5,0.5)"
+        }
     }
 
     function mousedownEvent(thisdownVal){
         xcordmd = event.clientX;
-        console.log(xcordmd)
     }
 
     function mouseupEvent(thisdownVal){
         xcoordmu = event.clientX;
-        console.log(xcoordmu)
-    }  
+        console.log('xcordmd' + xcordmd);
 
-    function mousemoveEvent(){
-        console.log('mousemove');
+        console.log('xcoordmu'  + xcoordmu);
+        if((xcordmd - xcoordmu) > 0){
+            if(counter < sliderListLength-1){
+                slideList.style.transform += "translateX(-300px)";
+                slideList.style.transition = "1s";
+                counter++;
+            }
+        }
+        else{
+            if(counter > 0){
+                slideList.style.transform += "translateX(300px)";
+                slideList.style.transition = "1s";
+                counter--;
+            }
+        }
     }
+
+    return{
+        zoomPic : zoomPic,
+        mousedownEvent : mousedownEvent,
+        mouseupEvent : mouseupEvent
+    }
+})();
