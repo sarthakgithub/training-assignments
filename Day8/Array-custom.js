fa59c7a0bc429486f7d9fd08008e59cacc945bcd
Sarthak Khandelwal (SAKH) <sakh@saxobank.com>
2017-07-24 09:02:18 +0530

    changes

---
+++ Day8/Array-custom.js
@@ -0,0 +1,20 @@
+Array.prototype.get = function(index) {
+    return this[index];
+}
+
+Array.prototype.set = function(index, value) {
+    this[index] = value;
+    return this;
+}
+
+Array.prototype.customLength = function(){
+    var count=0;
+    this.forEach(function(item,index){
+        if(typeof index === 'number'){
+            count++;
+        }
+    })
+    return count;
+}
+
+    [1,2,3].customLength();
